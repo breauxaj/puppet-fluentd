d66d88a4c94025633e37de51968ac22cffb5f012
Adam Breaux <breauxaj@gmail.com>
2015-01-31 13:42:01 -0500

    updated docs and file subscription

--- README.md
+++ README.md
@@ -5,6 +5,12 @@ Fluentd is an open source data collector for unified logging layer. Fluentd
 allows you to unify data collection and consumption for a better use and
 understanding of data.
 
+Notes
+-----
+
+Module does not attempt to manage the configuration in this version but does
+subscribe to the main configuration file to enable service synchronization.
+
 Samples
 -------
 ```
--- manifests/service.pp
+++ manifests/service.pp
@@ -6,9 +6,18 @@ define fluentd::service (
     /(?i-mx:centos|fedora|redhat|scientific)/ => 'td-agent',
   }
 
+  $config = $::operatingsystem ? {
+    /(?i-mx:centos|fedora|redhat|scientific)/ => '/etc/td-agent/td-agent.conf',
+  }
+
   service { $service:
-    ensure  => $ensure,
-    enable  => $enable,
+    ensure    => $ensure,
+    enable    => $enable,
+    subscribe => File[$config],
+  }
+
+  file { $config:
+    ensure => file,
   }
 
 }
